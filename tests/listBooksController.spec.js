8d1e357243682ef18dc744f34bc3726bfb5ec56f
davyengone <engonenzedavy@yahoo.com>
2016-03-04 05:36:32 +0100

    added few tests but still a lot to do so stay tuned

---
+++ tests/listBooksController.spec.js
@@ -0,0 +1,14 @@
+
+describe('Book Directive', ()=>{
+
+  beforeEach(module('testing.angular'));
+
+  beforeEach(inject(($compiler)=>{
+
+
+  }));
+
+  it('Should list all books', ()=>{
+    expect(2).toEqual(2);
+  });
+});
---
+++ tests/rootScope.spec.js
@@ -0,0 +1,19 @@
+// describe('rootScope', ()=>{
+//
+//   let MainController = ($scope) =>{
+//       $scope.books = [];
+//   };
+//
+//   let scope = {}, rootScope, controller;
+//
+//   beforeEach(inject(($controller, $rootScope)=>{
+//     let ctrl = $controller(MainController, {$scope: scope});
+//
+//       console.log(ctrl, scope);
+//   }));
+//
+//
+//   it('Should test rootscope', ()=>{
+//     expect(scope).toEqual({books: []});
+//   });
+// });
